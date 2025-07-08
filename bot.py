9042c0cf1e81d966d09394bf01eb439fedc43013
odiolascripto <angelmarcos1976@gmail.com>
2025-07-08 19:34:22 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,14 @@ from keep_alive import keep_alive
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+# 🔧 Esto es clave para evitar conflictos
+bot.delete_webhook(drop_pending_updates=True)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Bot activo 🚀")
 
-# ‼️ ESTE handler es solo para descubrir el ID del chat
+# ‼️ ESTE handler es solo para descubrir el ID del chat
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     chat_id = message.chat.id
