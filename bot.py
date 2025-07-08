90c8db1d069a874e28b2653b0f14d288d60f4ebe
odiolascripto <angelmarcos1976@gmail.com>
2025-07-08 19:23:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,8 @@
 import os
-import telebot
-from keep_alive import keep_alive
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN)
+url = f"https://api.telegram.org/bot{TOKEN}/deleteWebhook"
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Bot activo 🚀")
-
-# Handler para obtener el chat ID de cualquier grupo o chat donde envíes un mensaje
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    chat_id = message.chat.id
-    bot.reply_to(message, f"El chat ID de este grupo es: {chat_id}")
-
-keep_alive()
-bot.polling()
+response = requests.post(url)
+print(response.json())
